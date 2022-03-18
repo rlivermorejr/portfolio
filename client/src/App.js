61183d15e564e87be123e543b270e6b6b4b1d396
rlivermorejr <rlivermorejr@gmail.com>
2022-03-18 14:06:00 -0400

    initial

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
+function App() {
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<RecordList />} />
+				<Route path="/edit/:id" element={<Edit />} />
+				<Route path="/create" element={<Create />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
